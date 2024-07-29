11d7fe205273f4ccaea3fb4248dd073906c481d7
M M Danish <danishsadpara@gmail.com>
2024-07-29 21:12:33 +0500

    CHANGE: Dependencies

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,18 +1,8 @@
-import Hero from "@/components/Hero";
-import Avatar from "../components/Avatar";
-import Tag from "../components/Tag";
+"use client";
+import React from "react";
+import Hero from "@/sections/Hero";
 
 export default function Home() {
-  type optional<Type> = {
-    [Property in keyof Type]+?: Type[Property];
-  };
-  type user = {
-    name: string;
-    email: string;
-    password: string;
-    age: string;
-  };
-  type mutableUser = optional<user>;
   return (
     <main className="flex  flex-col items-center justify-between py-24">
       <Hero />
